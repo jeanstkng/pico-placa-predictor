c6f135141618b0c147a30366e251ea302889a40d
jeanstkng <jjg.castillo@yavirac.edu.ec>
2021-06-07 02:11:01 -0500

    Added dev dependencies and created component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PredictorComponent } from './predictor/predictor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PredictorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/predictor/predictor.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PredictorComponent } from './predictor.component';
+
+describe('PredictorComponent', () => {
+  let component: PredictorComponent;
+  let fixture: ComponentFixture<PredictorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PredictorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PredictorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
